cf9602aaba379efb78e64ba06fd6097308661817
cyc-func <yasharyang@gmail.com>
2017-06-16 13:59:04 +0530

    Create InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,30 @@
+class InsertionSort
+{
+	public static void main(String args[]) throws IOException
+	{
+		int A[] = {5, 1, 3, 4, 2};
+		InsertionSort(A);
+
+		for(int i : A)
+		{
+			System.out.println(i);
+		}
+	}
+
+	private static void InsertionSort(int A[])
+	{
+		for(int i = 1; i < A.length; i++)
+		{
+			int j = i - 1;
+			int sentinel = A[i];
+			
+			while(j >= 0 && A[j] > sentinel)
+			{
+				A[j + 1] = A[j];
+				j--;
+			}
+
+			A[j + 1] = sentinel;
+		}
+	}
+}
